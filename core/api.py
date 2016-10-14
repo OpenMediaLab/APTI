0d86212ec7e559943b41b41e809d366717d0e231
boddmg <boddmg@gmail.com>
2016-10-14 23:25:38 +0800

    Add a polar coordinate system transform.
    Signed-off-by: boddmg <boddmg@gmail.com>

--- core/api.py
+++ core/api.py
@@ -11,14 +11,14 @@ from utility import pcm2float, float2pcm
 image_width = 600.0
 fs = 44100
 frame_length = image_width / fs
-T = 600 * frame_length
+T = image_width * frame_length
 # T = 32.67 # sample time
 # frame_length = math.sqrt(T/float(fs))
 
 hop_length = frame_length # forget it
 
-normal = lambda x:(x+300)/600
-inv_normal = lambda x:x*600 - 300
+normal = lambda x:(x+50)/100
+inv_normal = lambda x:x*100 - 50
 
 def save_image(data, file_name):
     print(data.dtype, data.shape)
@@ -28,8 +28,17 @@ def save_image(data, file_name):
 def read_image(file_name):
     return tifffile.imread(file_name)
 
+def P2R(radius, angles):
+    return radius * np.exp(1j*angles)
+
+def R2P(x):
+    return np.absolute(x), np.angle(x)
+
+MAX = 2**8
+
 def audio2image(audio_filename, image_filename):
     rate, data =  scipy.io.wavfile.read(audio_filename)
+    print(data.dtype)
 
     if len(data.shape) > 1:
         data = data[:,0]
@@ -38,8 +47,15 @@ def audio2image(audio_filename, image_filename):
     x = data
     x = x[:int(T*rate)]
     X = stft.stft(x, fs, frame_length, hop_length)
-    X_image = cv2.merge([normal(X.real) * 65535, normal(X.imag) * 65535, np.zeros(X.shape[:2])])
-    X_image = X_image.astype('uint16')
+
+    print(np.max(X.real),np.min(X.real))
+    print(np.max(X.imag),np.min(X.imag))
+    r = normal(np.absolute(X)) * MAX
+    g = normal(np.angle(X)) * MAX
+    print(r, g)
+    X_image = cv2.merge([r, g, np.zeros(X.shape[:2])])
+    # X_image = cv2.merge([normal(X.real) * 65535, normal(X.imag) * 65535, np.zeros(X.shape[:2])])
+    X_image = X_image.astype('uint8')
     print("finished stft.")
     # Plot the magnitude spectrogram.
     save_image(X_image,image_filename)
@@ -49,13 +65,16 @@ def image2audio(image_filename, audio_filename):
     print("show result of stft.")
 
     print(X_image.dtype, X_image.shape)
-    new_X = np.zeros(X_image.shape[:2], 'complex128')
-    new_X.real = inv_normal(X_image[:,:,0].astype('float64') / 65535)
-    new_X.imag = inv_normal(X_image[:,:,1].astype('float64') / 65535)
-    print(np.max(new_X.real),np.min(new_X.real))
-    print(np.max(new_X.imag),np.min(new_X.imag))
+    r = inv_normal(X_image[:,:,0].astype('float64') / MAX)
+    g = inv_normal(X_image[:,:,1].astype('float64') / MAX)
+    X = P2R(r, g)
+    # X = np.zeros(X_image.shape[:2], 'complex128')
+    # X.real = r
+    # X.imag = g
+    print(np.max(X.real),np.min(X.real))
+    print(np.max(X.imag),np.min(X.imag))
     # Compute the ISTFT.
-    xhat = stft.istft(new_X, fs, T, hop_length)
+    xhat = stft.istft(X, fs, T, hop_length)
     xhat = float2pcm(xhat)
     scipy.io.wavfile.write(audio_filename, fs, xhat)
 
--- main.py
+++ main.py
@@ -12,15 +12,15 @@ __name__ = "__main__"
 from core import audio2image, image2audio
 
 if __name__ == "__main__":
-    # audio_file_name = "BWV-998-Aria"
-    # audio2image(audio_file_name+'.wav', audio_file_name+'.tiff')
-    # image2audio(audio_file_name + '.tiff', 'after-'+audio_file_name+'.wav')
-    #
-    # audio_file_name = "Duke Ellington - Rext"
-    # audio2image(audio_file_name+'.wav', audio_file_name+'.tiff')
-    # image2audio(audio_file_name + '.tiff', 'after-'+audio_file_name+'.wav')
+    audio_file_name = "BWV-998-Aria"
+    audio2image(audio_file_name+'.wav', audio_file_name+'.tiff')
+    image2audio(audio_file_name + '.tiff', 'after-'+audio_file_name+'.wav')
+    
+    audio_file_name = "Duke Ellington - Rext"
+    audio2image(audio_file_name+'.wav', audio_file_name+'.tiff')
+    image2audio(audio_file_name + '.tiff', 'after-'+audio_file_name+'.wav')
 
-    audio_file_name = "output"
+    audio_file_name = "out"
     image2audio(audio_file_name + '.tiff', 'after-'+audio_file_name+'.wav')
 
 
